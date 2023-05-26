c5b8250ce71018fa58f4a8a323f75aba43673de9
Christian Foster <35413427+Fynis96@users.noreply.github.com>
2023-05-26 12:27:43 -0400

    Big refactor, changed up the styling entirely, added in rainbow feature properly, messed about with animations using css.

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
-// TODO: Finish functionality for all buttons
-// TODO: add style to the page, spruce it up a bit
+let color = 'black';
+let isPaintingEnabled = true;
 
-
-
-let color = 'black'
-let Clicked = true;
-
-
-function populateBoard(size) 
-{
-    let board = document.querySelector('.grid');
-    let squares = board.querySelectorAll("div");
+function populateBoard(size) {
+    const board = document.querySelector('.grid');
+    const squares = board.querySelectorAll('div');
     squares.forEach((div) => div.remove());
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    board.style.gridTemplateRows = `repeat(${size} , 1fr)`
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    let amount = size * size;
-    for(let i = 0; i < amount; i++)
-    {
-        let square = document.createElement('div');
+    const amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        const square = document.createElement('div');
         square.style.backgroundColor = 'white';
-        square.addEventListener('mouseover', (e) => { if (Clicked == true){square.style.backgroundColor = color;}}); //Add a Clicked bool to toggle painting on and off
-        board.insertAdjacentElement('beforeend', square);
+        square.addEventListener('mouseover', (e) => {
+            if (isPaintingEnabled) {
+                if (color === 'rainbow') {
+                    square.style.backgroundColor = getRandomColor();
+                } else {
+                    square.style.backgroundColor = color;
+                }
+            }
+        });
+        board.appendChild(square);
     }
-
 }
 
-function changeSize(input)
-{   
-    if (input >= 4 & input <= 80)
-    {
-        populateBoard(input)
-    }
-    else
-    {
-        console.log('too much or too little')
+function changeSize(input) {
+    if (input >= 4 && input <= 80) {
+        populateBoard(input);
+    } else {
+        console.log('Invalid grid size');
     }
 }
 
 populateBoard(16);
 
-
-function clicked()
-{
-    if (Clicked == true)
-    {
-        Clicked = false;
-    }
-    else
-    {
-        Clicked = true;
-    }
+function togglePainting() {
+    isPaintingEnabled = !isPaintingEnabled;
 }
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+document.getElementById('black-btn').addEventListener('click', () => {
+    color = 'black';
+});
+
+document.getElementById('eraser-btn').addEventListener('click', () => {
+    color = 'white';
+});
+
+document.getElementById('rainbow-btn').addEventListener('click', () => {
+    color = 'rainbow';
+});
+
+document.getElementById('clear-btn').addEventListener('click', () => {
+    const gridSizeInput = document.getElementById('grid-size-input');
+    changeSize(parseInt(gridSizeInput.value));
+});
+
+document.getElementById('set-size-btn').addEventListener('click', () => {
+    const gridSizeInput = document.getElementById('grid-size-input');
+    changeSize(parseInt(gridSizeInput.value));
+});
